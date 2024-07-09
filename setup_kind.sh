5f3798db399d40ff7d1627dd8d095d5016856905
mrdubr <mr.dubr@gmail.com>
2024-07-09 12:05:00 -0400

    custom network name for the control plane container

--- setup_kind.sh
+++ setup_kind.sh
@@ -4,12 +4,16 @@ echo "Setting up kind cluster"
 
 kind delete cluster
 
-# Create a temporary kind config file in the SAME network as the client container
+# Create a temporary kind config file
 CONFIG_FILE=$(mktemp)
 cat <<EOF > "$CONFIG_FILE"
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 EOF
+
+# To allow `kind` to use the custom Docker network, I am relying on `KIND_EXPERIMENTAL_DOCKER_NETWORK`. 
+# See kubernetes-sigs/kind#273 and kubernetes-sigs/kind#1538.
+export KIND_EXPERIMENTAL_DOCKER_NETWORK=rundeck_network
 
 # If you're curious about the kind config
 cat "$CONFIG_FILE"
@@ -24,9 +28,7 @@ echo "Finished setting up kind cluster"
 # If you're curious about the cluster config
 # cat /root/.kube/config
 
-echo "Finished Setting up kind cluster"
-
-# Wait for the client to finish
+# Wait for the client to finish its work
 # Loop as long as python-client is pingable
 while ping -c 1 python-client &> /dev/null; do
     echo "python-client is still reachable"
